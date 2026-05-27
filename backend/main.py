6d02ef8b2a1c7443251a6a92664920a2d8626442
Shobhit Srivastava <shobhitsri1405@gmail.com>
2026-05-27 18:58:42 +0530

    changed embedding model

--- backend/main.py
+++ backend/main.py
@@ -56,7 +56,9 @@ llm = ChatOpenAI(
 
 
 # embedding model
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/paraphrase-MiniLM-L3-v2"
+    )
 
 # CORS middleware
 app.add_middleware(
